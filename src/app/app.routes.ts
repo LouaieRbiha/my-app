64181e2a45077ef2dfd220583928ca6aadf471cb
Louaie Rbiha <louaierbiha@gmail.com>
2025-10-08 16:30:36 +0100

    feat: add layout structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Route } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
 import { Home } from './features/home/home';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: Dashboard,
   },
 ];
